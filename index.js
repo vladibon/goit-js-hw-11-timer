17467d5204e6abc14107e0331775f602f8c0103e
Vladimir Bondar <vladi.bondar@gmail.com>
2023-02-04 20:21:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import CountdownTimer from './js/timer.js';
 
 new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date(2021, 9, 6, 19, 30),
+  targetDate: new Date(2085, 3, 19, 00, 00),
 });
